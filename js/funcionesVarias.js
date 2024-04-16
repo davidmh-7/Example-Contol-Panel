1547492dbe6151ae200815dc41c13d5ad7cb11c3
David Mangas Hernandez <148961050+davidmh-7@users.noreply.github.com>
2024-04-16 12:30:59 +0200

    Add files via upload

--- js/funcionesVarias.js
+++ js/funcionesVarias.js
@@ -8,6 +8,12 @@ const bajarLetra = document.getElementById('bajarLetra');
 const letrasCard = document.getElementById('letrasCard');
 const tamanomovil = document.getElementById('tamanomovil');
 const mostrarTamano = document.getElementById('mostrarTamano');
+const logoIrun = document.getElementById('logoIrun');
+const menuDesplegable = document.getElementById('menuDesplegable');
+const quitarMenuMovil2 = document.getElementById('quitarMenuMovil2');
+
+
+
 
 function verificarAnchoVentana() {
     // Obtener el ancho de la ventana del navegador
@@ -22,16 +28,20 @@ function verificarAnchoVentana() {
         CuadradoGrande.classList.add('absolute', 'top-0', 'left-0', 'right-0', 'bottom-0', 'bg-gray-50', 'overflow-hidden');
 
         bajarLetra.style.display = 'text-xs';
-        letrasCard.style.display = 'text-xs';
-        mostrarTamano.style.display = 'none'
+        // letrasCard.style.display = 'text-xs';
+        mostrarTamano.style.display = 'none';
         tamanomovil.style.display = 'block';
+
+        // menu Desplegable
+        menuDesplegable.style.display = 'block';
+        logoIrun.classList.remove('mt-4');
+        logoIrun.classList.add('mt-24','absolute');
       
     }
 
     if (anchoVentana <= 640){
         console.log("La ventana es igual o menor a 640px");
-        quitarParaMovil.style.display = 'none';
-        quitarMenuMovil.style.display = 'none';
+        
 
         left64Element.classList.remove('left-64');
         CuadradoGrande.removeAttribute('style');
@@ -43,7 +53,13 @@ function verificarAnchoVentana() {
 
         tamanomovil.style.display = 'none';
         mostrarTamano.style.display = 'block';
-
+        
+        // menu Desplegable
+        menuDesplegable.style.display = 'block';
+        logoIrun.classList.remove('mt-4');
+        logoIrun.classList.add('mt-24','absolute');
+     
+    
 
     }
    
@@ -63,8 +79,35 @@ function verificarAnchoVentana() {
 
         mostrarTamano.style.display = 'none'
         tamanomovil.style.display = 'block';
+        
+        // menu Desplegable
+        menuDesplegable.style.display = 'none';
+        logoIrun.classList.remove('mt-24','absolute');
+        logoIrun.classList.add('mt-4');
+    }
+    
+
+
+
+}
+
+let quitarMostrarMenu = 0;
+
+function menuDesplegablee() {
+    console.log(quitarMostrarMenu);
+    // Si quitarMostrarMenu es divisible por 2 y da 0, muestra el menú
+    if (quitarMostrarMenu % 2 === 0) {
+        quitarMenuMovil.style.display = 'block';
+        // Incrementa el contador
+        quitarMostrarMenu++;
+    } else {
+        // Si quitarMostrarMenu no es divisible por 2 y da un número diferente de 0, oculta el menú
+        quitarMenuMovil.style.display = 'none';
+        // Incrementa el contador
+        quitarMostrarMenu++;
     }
 }
+
 // Llamar a la función al cargar la página y cada vez que se redimensione la ventana
 window.addEventListener("load", verificarAnchoVentana);
 window.addEventListener("resize", verificarAnchoVentana);
